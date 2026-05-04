0cf2d48ccfb890e091d652c3f56353fdbf642aab
unknown <annisaazzahraputrizahra@gmail.com>
2026-05-05 06:44:12 +0700

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace KPL_MOD10_SE_48_02_103022400016_ENH
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
